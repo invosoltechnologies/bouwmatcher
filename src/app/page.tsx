bf49316188052b5a35943223587d86647951786a
Muneeb Chaudhry <muneebchaudhry.dev@gmail.com>
2025-09-14 21:37:59 +0500

    Add Trusted PArtner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import CTASection from "@/components/Homepage/CTASection";
 import FAQSection from "@/components/Homepage/FAQSection";
 import ReviewsSection from "@/components/Homepage/ReviewsSection";
 import StatsSection from "@/components/Homepage/StatsSection";
+import PartnersSection from "@/components/Homepage/PartnersSection";
 
 export default function Home() {
   return (
 <>
 <Hero />
+<PartnersSection />
 <StatsSection />
 <ReviewsSection />
 <FAQSection />
