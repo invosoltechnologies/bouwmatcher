3def7325215fe6ff8ef3c00d1283dec9b79755d0
Muneeb Chaudhry <muneebchaudhry.dev@gmail.com>
2025-09-14 18:52:25 +0500

    add the stats section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Hero from "@/components/Homepage/Hero";
 import CTASection from "@/components/Homepage/CTASection";
 import FAQSection from "@/components/Homepage/FAQSection";
 import ReviewsSection from "@/components/Homepage/ReviewsSection";
+import StatsSection from "@/components/Homepage/StatsSection";
 
 export default function Home() {
   return (
 <>
 <Hero />
+<StatsSection />
 <ReviewsSection />
 <FAQSection />
 <CTASection />
