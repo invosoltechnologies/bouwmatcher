2712138a0d63dba2d4a938a8e61c4bc952fe4ddf
Muneeb Chaudhry <muneebchaudhry.dev@gmail.com>
2025-09-19 11:29:46 +0500

    update the legal

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -1,10 +1,12 @@
 import HeroLegal from "@/components/Legal/HeroLegal";
+import LegalDescription from "@/components/Legal/LegalDescription";
 
 
 export default function Legal() {
   return (
     <>
       <HeroLegal />
+      <LegalDescription/>
     </>
   );
 }
