0abafe56fc5ba0503f0620b22b6eb6641cb1c366
Muneeb Chaudhry <muneebchaudhry.dev@gmail.com>
2025-09-28 10:36:09 +0500

    Update the privacy page

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import HeroLegal from "@/components/PrivacyPolicy/HeroLegal";
+import PrivacyDescription from "@/components/PrivacyPolicy/PrivacyDescription";
+
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <HeroLegal />
+      <PrivacyDescription/>
+    </>
+  );
+}
