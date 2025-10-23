8707d307df8411aa5227f2a27d7cecd55832960d
Muneeb Chaudhry <muneebchaudhry.dev@gmail.com>
2025-10-23 20:59:15 +0500

    Deleted the Login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,7 @@
-import AuthLayout from "@/components/auth/AuthLayout";
-import LoginForm from "@/components/auth/LoginForm";
+
 
 export default function LoginPage() {
   return (
-    <AuthLayout>
-      <LoginForm />
-    </AuthLayout>
+<></>
   );
 }
